ec5a99915595e7d003f489579b5de01894bc37d1
alaaaldabak <alaa_aldabak@hotmail.com>
2021-07-02 19:45:16 +0300

    hesap makinesi

---
+++ src/HesapMakinesi.java
@@ -0,0 +1,36 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main( String[] args){
+        int n1,n2,select;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ilk Syayıyı giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("Ikinci Syayıyı giriniz: ");
+        n2 = input.nextInt();
+        System.out.println("1-Toplam\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.println("Seçeniz:");
+        select = input.nextInt();
+        switch (select){
+            case 1 :
+                System.out.println("Toplam  "+(n1+n2));
+                break;
+            case 2 :
+                System.out.println("Çıkarma  "+(n1-n2));
+                break;
+            case 3 :
+                System.out.println("Çarpma  "+(n1*n2));
+                break;
+            case 4 :
+                if(n2 != 0 ){
+                    System.out.println("Bölme  "+(n1/n2));
+                }else
+                   System.out.println("Bir sayı 0'a bölünemez!  ");
+                break;
+            default:
+                System.out.println("Yanlış seçin yaptınız: Tekrar deneyiniz:  ");
+        }
+    }
+}
